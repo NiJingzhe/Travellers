a7f78b490cc9624fc71353358fefc1513f375c5e
ni-jingzhe <1448976235@qq.com>
2023-07-05 00:24:26 +0800

    完成基本测试场景搭建和基础人物动画与移动

---
+++ script/player/player.gd
@@ -0,0 +1,44 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+@onready var animation_tree : AnimationTree = $AnimationTree
+@onready var animation_player : AnimationPlayer = $AnimationPlayer
+@onready var animation_state = animation_tree.get('parameters/playback')
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	animation_tree.active = true
+	animation_player.speed_scale = 5
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down")
+	if input_dir != Vector2.ZERO:
+		animation_tree.set('parameters/Idle/blend_position', input_dir)
+		animation_tree.set('parameters/Walk/blend_position', input_dir)
+		animation_state.travel('Walk')
+	else:
+		animation_state.travel('Idle')
+	var direction = (transform.basis * Vector3(input_dir.y, 0, -input_dir.x)).normalized()
+	
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
