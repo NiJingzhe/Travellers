ea5f929a9b33c302e937c481e20a49e6f9c27f1a
ni-jingzhe <1448976235@qq.com>
2023-07-11 15:49:43 +0800

    基本完成昼夜更替视效框架,规范了部分脚本的命名，添加了m便于美工使用的说明和注释

--- script/DataManager/DataManager.gd
+++ script/DataManager/DataManager.gd
@@ -12,3 +12,5 @@ class_name DataManager
 
 
 
+
+
--- script/TimeSystem/SunSystem.gd
+++ script/TimeSystem/SunSystem.gd
@@ -2,16 +2,25 @@
 extends Node3D
 class_name SunSystem
 
+#Energy Curve是light_energy关于game_time的函数，横坐标0-1对应game_time的0-24
+
+#Shadow Blur Curve和Shadow Transparency Curve是阴影的模糊程度和透明度关于光强的函数
+
+#Angle Curve比较特殊，虽然也是太阳角度关于game_time的函数，但是为了正确的处理间断点，
+#整体曲线向左平移了0.25，即6 hours，这是为了让间断点产生在曲线边界，
+#即其横坐标0-0.75对应的是game_time的6-24，0.75-1对应game_time的0-6，自定义曲线时要注意这点
+
 @export var energy_curve : Curve
+@export var angle_curve : Curve      #整体向左侧平移了0.25个循环
+@export var shadow_blur_curve : Curve
+@export var shadow_transparency_curve : Curve
 @export var dawntime_color : Color = Color(0,0,0,1)
 @export var daytime_color : Color = Color(1, 1, 1, 1)
 @export var dusktime_color : Color = Color(0,0,0,1)
 @export var nighttime_color : Color = Color(0.2, 0.2, 0.5, 1)
 @export var time_of_whole_day : float
 @export var game_time : float = 12.0
-@export var angle_curve : Curve      #整体向左侧平移了0.25个循环
-@export var shadow_blur_curve : Curve
-@export var shadow_transparency_curve : Curve
+
 
 
 # Called when the node enters the scene tree for the first time.
