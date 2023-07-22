4fb18ac701407b7e183cb81a0f6d9d70d1de6663
ni-jingzhe <1448976235@qq.com>
2023-07-22 19:07:03 +0800

    清除部分场景继承关系

--- script/player/player.gd
+++ script/player/player.gd
@@ -38,6 +38,7 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
